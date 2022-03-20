103192c369cce3d92d3bba64077ca105e4dbf58e
mi1an <jadavmilan009@gmail.com>
2022-03-20 22:56:16 +0530

    changes

---
+++ Practical23/Default.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2">Name</td>
+                    <td class="auto-style3">
+                        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">City</td>
+                    <td class="auto-style3">
+                        <asp:TextBox ID="txtCity" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Country</td>
+                    <td class="auto-style3">
+                        <asp:TextBox ID="txtCountry" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                
+            </table>
+            
+            <asp:Button ID="btnInsert" runat="server" OnClick="btnInsert_Click" Text="Insert" />
+&nbsp;&nbsp;&nbsp;
+            
+            <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click"/>
+            &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+            <br />
+            <asp:Label ID="lblCheck" runat="server" Text="Label" Visible="False"></asp:Label>
+            <br />
+            <asp:GridView ID="GridView1" runat="server" >
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Practical23/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+public partial class _Default : System.Web.UI.Page
+{
+    static string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\mi1an\source\repos\SEM6\advdotnet\Practical12\App_Data\Database.mdf;Integrated Security=True";
+    SqlConnection con = new SqlConnection(cs);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+            con.Open();
+            string query = "SELECT * from users";
+
+            SqlCommand cmdr = new SqlCommand(query, con);
+            SqlDataReader reader = cmdr.ExecuteReader();
+
+            GridView1.DataSource = reader;
+            GridView1.DataBind();
+       
+            reader.Close();
+
+    }
+
+    protected void btnInsert_Click(object sender, EventArgs e)
+    {
+            string query = "insert into users(name, city, country) values('" + txtName.Text + "',  '" + txtCity.Text + "', '" + txtCountry.Text + "')";
+            SqlCommand cmd = new SqlCommand(query, con);
+            int check = cmd.ExecuteNonQuery();
+            if (check > 0)
+            {
+                lblCheck.Text = "Data Inserted";
+            lblCheck.Visible = true;
+            }
+    }
+    protected void btnUpdate_Click(object sender, EventArgs e)
+    {
+        string query = "UPDATE users SET name='" + txtName.Text + "', city='" + txtCity.Text + "', country='" + txtCountry.Text + "' WHERE Name='" + txtName.Text + "'";
+        SqlCommand cmd = new SqlCommand(query, con);
+        int check = cmd.ExecuteNonQuery();
+        if (check > 0)
+        {
+            lblCheck.Text = "Data Updated";
+            lblCheck.Visible = true;
+        }
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        string query = "DELETE FROM users WHERE name = ('"+txtName.Text+"') ";
+        SqlCommand cmd = new SqlCommand(query, con);
+        int check = cmd.ExecuteNonQuery();
+        if (check > 0)
+        {
+            lblCheck.Text = "Data Deleted";
+            lblCheck.Visible = true;
+        }
+    }
+}
